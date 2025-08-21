8e20e775a67cfae9752d07ef07dd55bc215192d9
Caio T <caio.trocilo@gmail.com>
2025-08-21 10:59:48 -0300

    https://angular.dev/tutorials/: Your first Angular app: Add search functionality

--- src/app/components/details/details.ts
+++ src/app/components/details/details.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Housing } from '../../housing';
+import { HousingLocationInfo } from '../../housinglocation';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './details.html',
   styleUrl: './details.css'
 })
 export class Details {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(Housing)
+  housingLocation: HousingLocationInfo | undefined;
 
+  housingLocationId = -1;
+
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
+  }
 }
